0ab141e8f407c597d0c0c2f62b7626908d09adb4
pride2602 <107101181+pride2602@users.noreply.github.com>
2023-06-10 21:42:47 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,66 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <mysql.h>
+
+using namespace std;
+
+int main() {
+	MYSQL mysql;
+	MYSQL_RES* res;
+	MYSQL_ROW row;
+
+	int i = 0;
+
+	// Получаем дескриптор соединения
+	mysql_init(&mysql);
+	if (&mysql == nullptr) {
+		// Если дескриптор не получен — выводим сообщение об ошибке
+		cout << "Error: can't create MySQL-descriptor" << endl;
+	}
+
+	// Подключаемся к серверу
+	if (!mysql_real_connect(&mysql, "localhost", "root", "honter77", "testdb", NULL, NULL, 0)) {
+		// Если нет возможности установить соединение с БД выводим сообщение об ошибке
+		cout << "Error: can't connect to database " << mysql_error(&mysql) << endl;
+	}
+
+	mysql_query(&mysql, "CREATE TABLE table_fromCplusplus(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))");
+	mysql_query(&mysql, "INSERT INTO table_fromCplusplus(id, name) values(default, 'Daria')");
+	mysql_query(&mysql, "INSERT INTO table_fromCplusplus(id, name) values(default, 'Lida')");
+	mysql_query(&mysql, "INSERT INTO table_fromCplusplus(id, name) values(default, 'Nastya')");
+
+
+	mysql_query(&mysql, "SELECT * FROM table_fromCplusplus"); //Делаем запрос к таблице
+
+	//Выводим все что есть в таблице через цикл
+	if (res = mysql_store_result(&mysql)) {
+		while (row = mysql_fetch_row(res)) {
+			for (i = 0; i < mysql_num_fields(res); i++) {
+				cout << row[i] << "  ";
+			}
+			cout << endl;
+		}
+	}
+	else
+		cout << "Ошибка MySql номер " << mysql_error(&mysql);
+
+	// Закрываем соединение с базой данных
+	mysql_close(&mysql);
+
+	system("Pause");
+
+	return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
